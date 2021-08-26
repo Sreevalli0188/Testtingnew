2a976de319c6c525d2be99f9ab30e283ca883b6d
Shivanand <shivuas123@gmail.com>
2021-08-26 15:08:19 +0530

    first commit

---
+++ AbstractionExample.java
@@ -0,0 +1,26 @@
+
+public class AbstractionExample {
+	
+	public int add(int a, int b)
+	{
+		System.out.println("Add");
+		return 1;
+	}
+	public float add(float a, int b)
+	{
+		System.out.println("Add");
+		return 1;
+	}
+	
+	public  float add(float a, float f)
+	{
+		return 1;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
